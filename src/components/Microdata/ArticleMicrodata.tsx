031536053b4c7492ecf92da1bf9ccb37b16423d5
Mykhailo Aliinyk <m.aliynik@gmail.com>
2023-03-08 21:25:27 -0500

    feat: improved post microdata content

--- src/components/Microdata/ArticleMicrodata.tsx
+++ src/components/Microdata/ArticleMicrodata.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
 type ArticleMicrodataProps = {
   type: 'Tech';
   name: string;
   body: string;
-  proficiencyLevel?: 'Expert';
+  headline?: string;
+  articleSection?: string;
+  dependencies?: string[];
+  proficiencyLevel?: 'Beginner' | 'Expert';
 };
 
-export const ArticleMicrodata = ({ type, name, body, proficiencyLevel }: ArticleMicrodataProps) => {
+export const ArticleMicrodata = ({
+  children,
+  type,
+  name,
+  headline,
+  articleSection,
+  dependencies,
+  body,
+  proficiencyLevel,
+}: PropsWithChildren<ArticleMicrodataProps>) => {
+  const countWords = body.split(' ').filter(word => word).length;
+
   return (
     <div itemType={`https://schema.org/${type}Article`} itemScope>
       <meta itemProp="name" content={name} />
       <meta itemProp="articleBody" content={body} />
+      <meta itemProp="wordCount" content={countWords.toString()} />
 
+      {headline && <meta itemProp="headline" content={headline} />}
+      {articleSection && <meta itemProp="articleSection" content={articleSection} />}
+      {dependencies && <meta itemProp="dependencies" content={dependencies.join(', ')} />}
       {proficiencyLevel && <meta itemProp="proficiencyLevel" content={proficiencyLevel} />}
+
+      {children}
     </div>
   );
 };
--- src/pages/ViewPostPage/ViewPostPageMicrodata.tsx
+++ src/pages/ViewPostPage/ViewPostPageMicrodata.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import type { Post } from '~/types';
 
-import { ArticleMicrodata } from '~/components';
+import { ArticleMicrodata, PersonMicrodata } from '~/components';
+import { MY_CURRENT_POSITION, MY_EMAIL, MY_FULL_NAME, MY_PHOTO } from '~/constants';
+
+const innerText = (element: Node) => {
+  const getTextLoop = (element: Node) => {
+    const texts: string[] = [];
+
+    Array.from(element.childNodes).forEach(node => {
+      if (node.nodeType === Node.TEXT_NODE) {
+        texts.push(node.textContent?.trim() ?? '');
+      } else {
+        texts.push(...getTextLoop(node));
+      }
+    });
+
+    return texts;
+  };
+
+  return getTextLoop(element).join(' ');
+};
 
 type ViewPostPageMicrodataProps = {
   post: Post;
 };
 
 export const ViewPostPageMicrodata = ({ post }: ViewPostPageMicrodataProps) => {
+  const textContent = useMemo(() => {
+    const content = window.document.createElement('div');
+
+    content.innerHTML = post.content;
+
+    return innerText(content);
+  }, [post.content]);
+
   return (
-    <ArticleMicrodata type="Tech" name={post.title} body={post.content} proficiencyLevel="Expert" />
+    <ArticleMicrodata type="Tech" name={post.title} body={textContent} proficiencyLevel="Expert">
+      <PersonMicrodata
+        name={MY_FULL_NAME}
+        email={MY_EMAIL}
+        jobTitle={MY_CURRENT_POSITION}
+        image={MY_PHOTO}
+        gender="Male"
+        itemProp="author"
+      />
+    </ArticleMicrodata>
   );
 };
